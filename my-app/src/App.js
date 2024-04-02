6a4dd30168d366b49779a3133704e25014af8d6e
ioaHmaN <98122963+namlnh10@users.noreply.github.com>
2024-04-02 10:07:46 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,28 @@
 // import React, { useState, useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+// import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
+// import Login from './Components/Login';
+// import Signup from './Components/Signup';
 import { useEffect, useState } from 'react';
 import HomePage from './Components/Page/HomePage';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState( );
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
+    console.log(token);
     if (token) {
       setIsLoggedIn(true);
+
     }
     setIsLoggedIn(false);
-  })
+  },[])
 
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={isLoggedIn ? <Header /> : <HomePage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Signup />} />
-      </Routes>
+      {isLoggedIn ? <HomePage /> : <Header />}
+      <Home/>
     </div>
   );
 };
--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,43 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import styles from "./Header.module.css";
-
+import Login from "./Login"
+import Signup from "./Signup"
 export default function Header() {
   return (
     <Container>
-      <NavLink
-        style={{ color: "#5cb85c", fontSize: "30px", textDecoration: "none" }}
-        to="/"
-      >
-        <strong>Conduit</strong>
-      </NavLink>
-      <div>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
-          }
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
-          }
-          to="/login"
-        >
-          Sign in
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
-          }
-          to="/register"
-        >
-          Sign up
-        </NavLink>
-      </div>
+      <Routes>
+        <Route path="/" element={ <Header /> } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+      </Routes>
     </Container>
   );
 }
