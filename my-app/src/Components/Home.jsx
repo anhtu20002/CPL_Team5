b1703d0becc15581b9c84bab8755d12e7a8ab4c8
anhtu20002 <108037940+anhtu20002@users.noreply.github.com>
2024-04-02 14:35:16 +0700

    update-home

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -18,21 +18,62 @@ const Home = () => {
       });
   }, []);
 
+  // format date
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { month: "long", day: "numeric", year: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  };
+
   return (
-    <div className="banner">
-      <div className="container">
-        <h1 className="logo-font">conduit</h1>
-        <p>A place to share your knowledge.</p>
+    <div>
+      <div
+        className="container-fluid py-4 text-center"
+        style={{
+          color: "white",
+          backgroundColor: "#5cb85c",
+        }}
+      >
+        <b className="logo-font" style={{ fontSize: "56px" }}>
+          conduit
+        </b>
+        <p style={{ fontSize: "24px" }}>A place to share your knowledge.</p>
       </div>
 
       <Container>
-        {articles.map((item, idex) => {
-          return (
-            <div>
-              <p>Title: {item.slug}</p>
-            </div>
-          );
-        })}
+        <Row>
+          <Col md={9}>
+            <Row>Global Feed</Row>
+            {articles.map((article, index) => {
+              return (
+                <div>
+                  <div className="author-info d-flex justify-content-between">
+                    <div className="d-flex">
+                      <a href="#">
+                        <img src={article.author.image} alt="" />
+                      </a>
+                      <div>
+                        <a href="#">{article.author.username}</a>
+                        <span>{formatDate(article.createdAt)}</span>
+                      </div>
+                    </div>
+                    <button>
+                      <i className="icon-heart"></i>
+                      {article.favoritesCount}
+                    </button>
+                  </div>
+
+                  <div className="article-info">
+                    <a href="#" style={{ textDecoration: "none" }}>
+                      <h3>{article.title}</h3>
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+          <Col md={3}>tag</Col>
+        </Row>
       </Container>
     </div>
   );
