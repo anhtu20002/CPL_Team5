423ea54858e4910d40a999a0bc958700648505e6
ioaHmaN <98122963+namlnh10@users.noreply.github.com>
2024-04-05 11:07:11 +0700

    update

--- my-app/src/Components/Page/Article.jsx
+++ my-app/src/Components/Page/Article.jsx
@@ -1,97 +1,133 @@
-import React , {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
 export default function Article() {
-    const token = localStorage.getItem("token");
-    const SERVER_API = "https://api.realworld.io/api";
-    console.log(token);
-    const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [about, setAbout] = useState('');
-  const [tags, setTags] = useState('');
+  const token = localStorage.getItem("token");
+  const SERVER_API = "https://api.realworld.io/api";
+  const [formData, setFormData] = useState({
+    title: "11",
+    content: "11",
+    about: "11",
+    tags: "11",
+  });
 
-  const handleTitleChange = (event) => {
-      setTitle(event.target.value)   
-    }
-     const handleContentChange = (event) => {
-       setContent(event.target.value);
-     };
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [successMessage, setSuccessMessage] = useState("");
 
-     const handleAboutChange = (event) => {
-       setAbout(event.target.value);
-     };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  console.log(formData);
+  const postwithToken = async (url, token, data) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("Data: ",data);
+    return response.json();
+  };
 
-     const handleTagsChange = (event) => {
-       setTags(event.target.value);
-    };
-    console.log(title, content, about, tags);
-    //Hàm gọi data
-    const postwithToken = async (url, accessToken, data) => {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify(data),
-      });
-        console.log(data);
-      return response.json();
-    };
-
-    //Hàm submit
-    const handleSubmit = async (event) => {
+  // Handler khi người dùng ấn nút đăng nhập
+  const handleSubmit = async  (event) => {
     event.preventDefault();
 
-      console.log(title,content,about,tags);
-    const tagArray = tags.split(',');
-        console.log(tagArray);
-  try {
-    // Gọi API đăng nhập sử dụng phương thức postwithToken
-    const { articles } = await postwithToken(`${SERVER_API}/articles`, token, {
-      article: {
-            title: title,
-        description: about,
-        body:content,
-        tagList: tagArray,
-      },
-    });
-
-    console.log(articles);
-    if (articles === undefined) {
-      console.error("Thêm bài viết thất bại 1");
-    } else {
-      console.log("Thêm bài viết thành công!", articles);
-      navigate("/");
+    try {
+      // Gọi API đăng nhập sử dụng phương thức postwithToken
+     const response = await postwithToken(`${SERVER_API}/articles`, token, {
+       article: formData,
+     });
+     console.log(response.data);
+     if (response.article) {
+       console.log("Đăng bài thành công!", response.article.title);
+       // Thực hiện các thao tác tiếp theo sau khi đăng bài thành công
+     } else {
+       console.error("Không có thông tin bài viết trả về từ máy chủ.");
+     }
+      // Xử lý khi đăng nhập thành công
+    } catch (error) {
+      // Xử lý khi đăng nhập thất bại
+      console.log(error);
     }
-  } catch (error) {
-    console.error("Thêm bài viết thất bại:", error);
-  }
-};
+  };
 
-    return (
-      <div className="container">
+ 
+  return (
+    <div className="container">
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" value={title} onChange={handleTitleChange} required />
-       </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">About: </label>
-          <input type="text" className="form-control" id="exampleInputEmail1"  value={about} onChange={handleAboutChange} required />
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <div className='mb-3'>
-            <div class="form-outline">
-                <textarea class="form-control" id="textAreaExample2" rows="8" value={content} onChange={handleContentChange}></textarea>
-                <label class="form-label" for="textAreaExample2">Message</label>
-            </div>
-        </div>        
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Tags:</label>
-          <input type="text" className="form-control" id="exampleInputPassword1" value={tags} onChange={handleTagsChange} required />
+          <label htmlFor="about" className="form-label">
+            About
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="about"
+            name="about"
+            value={formData.about}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <div className="mb-3">
+          <label htmlFor="content" className="form-label">
+            Content
+          </label>
+          <textarea
+            className="form-control"
+            id="content"
+            name="content"
+            rows="8"
+            value={formData.content}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="tags" className="form-label">
+            Tags
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tags"
+            name="tags"
+            value={formData.tags}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        {/* {errorMessage && (
+          <div className="alert alert-danger">{errorMessage}</div>
+        )}
+        {successMessage && (
+          <div className="alert alert-success">{successMessage}</div>
+        )} */}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
-};
+}
--- my-app/src/Components/Signup.jsx
+++ my-app/src/Components/Signup.jsx
@@ -1,48 +1,113 @@
-import React from 'react';
-
+import React , { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 const Signup = () => {
-    return (
-      <div>
-        <div className="container-login">
-          <div className="container-left">
-            <h2>Đăng kí</h2>
-            <p>Vui lòng nhập email và password của bạn</p>
-          </div>
-          <div className="container-form">
-            <form>
-              <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Re-Password"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-          </div>
+  const SERVER_API = "https://api.realworld.io/api";
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  console.log(formData);
+
+   const postwithToken = async (url, data) => {
+     const response = await fetch(url, {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+        //  Authorization: `Bearer ${accessToken}`,
+       },
+       body: data,
+     });
+     console.log(data);
+     return response.json();
+   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Gọi API đăng ký sử dụng phương thức postwithToken
+      const response = await postwithToken(`${SERVER_API}/users`, {
+        users:formData
+      });
+      // Kiểm tra xem yêu cầu đã được thực hiện thành công hay không
+      if (response.ok) {
+        // Nếu thành công, chuyển hướng đến trang chính
+        navigate("/");
+      } else {
+        // Nếu không thành công, xử lý lỗi từ server
+        // const errorMessage = await response.text();
+        // throw new Error(errorMessage);
+        console.log("lỗi");
+      }
+    } catch (error) {
+      // Xử lý lỗi từ phía client hoặc server
+      console.error("Đăng ký thất bại:", error);
+      alert("Đã xảy ra lỗi khi đăng ký.");
+    }
+  };
+
+  return (
+    <div>
+      <div className="container-login">
+        <div className="container-left">
+          <h2>Đăng kí</h2>
+          <p>Vui lòng nhập thông tin của bạn</p>
+        </div>
+        <div className="container-form">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
-
 export default Signup;
